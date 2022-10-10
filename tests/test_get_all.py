ba4892bd2ad069463ff1a6338661852667026ba1
Jakub Obstarczyk <jakub.obstarczyk@enjin.io>
2022-10-10 12:08:23 +0200

    added test_get_all.py

---
+++ tests/test_get_all.py
@@ -0,0 +1,21 @@
+import requests
+from constants import Url
+from constants import Endpoint
+from constants import StatusCode
+
+
+def test_get_all_is_successful():
+    response = requests.get(Url.SWAPI_DEV_API)
+    json_body = response.json()
+
+    assert response.status_code == StatusCode.OK_200
+    assert json_body['films'] == Endpoint.FILMS
+    assert json_body['people'] == Endpoint.PEOPLE
+    assert json_body['planets'] == Endpoint.PLANETS
+    assert json_body['species'] == Endpoint.SPECIES
+    assert json_body['starships'] == Endpoint.STARSHIPS
+    assert json_body['vehicles'] == Endpoint.VEHICLES
+
+def test_post_all_is_not_allowed():
+    response = requests.post(Url.SWAPI_DEV_API)
+    assert response.status_code == StatusCode.METHOD_NOT_ALLOWED_405
