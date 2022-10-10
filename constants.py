035aa13efe7ecbc5c7f54056a6465b7cab548b86
Jakub Obstarczyk <jakub.obstarczyk@enjin.io>
2022-10-10 12:29:18 +0200

    small clean up

--- constants.py
+++ constants.py
@@ -1,13 +1,11 @@
-class Url:
-    SWAPI_DEV_API = "https://swapi.dev/api/"
-
 class Endpoint:
-    FILMS = f'{Url.SWAPI_DEV_API}films/'
-    PEOPLE = f'{Url.SWAPI_DEV_API}people/'
-    PLANETS = f'{Url.SWAPI_DEV_API}planets/'
-    SPECIES = f'{Url.SWAPI_DEV_API}species/'
-    STARSHIPS = f'{Url.SWAPI_DEV_API}starships/'
-    VEHICLES = f'{Url.SWAPI_DEV_API}vehicles/'
+    SWAPI_DEV_API = "https://swapi.dev/api/"
+    FILMS = f'{SWAPI_DEV_API}films/'
+    PEOPLE = f'{SWAPI_DEV_API}people/'
+    PLANETS = f'{SWAPI_DEV_API}planets/'
+    SPECIES = f'{SWAPI_DEV_API}species/'
+    STARSHIPS = f'{SWAPI_DEV_API}starships/'
+    VEHICLES = f'{SWAPI_DEV_API}vehicles/'
 
 class StatusCode:
     METHOD_NOT_ALLOWED_405 = 405
--- tests/test_get_all.py
+++ tests/test_get_all.py
@@ -1,11 +1,10 @@
 import requests
-from constants import Url
 from constants import Endpoint
 from constants import StatusCode
 
 
 def test_get_all_is_successful():
-    response = requests.get(Url.SWAPI_DEV_API)
+    response = requests.get(Endpoint.SWAPI_DEV_API)
     json_body = response.json()
 
     assert response.status_code == StatusCode.OK_200
@@ -17,5 +16,5 @@ def test_get_all_is_successful():
     assert json_body['vehicles'] == Endpoint.VEHICLES
 
 def test_post_all_is_not_allowed():
-    response = requests.post(Url.SWAPI_DEV_API)
+    response = requests.post(Endpoint.SWAPI_DEV_API)
     assert response.status_code == StatusCode.METHOD_NOT_ALLOWED_405
