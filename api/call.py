7d5a89d21f745ffc7b897b76dbc9d5aaceb90ed4
Jakub Obstarczyk <jakub.obstarczyk@enjin.io>
2022-10-11 12:00:01 +0200

    new function get_by_endpoint() in call.py

--- api/call.py
+++ api/call.py
@@ -1,12 +1,22 @@
 from api.calls.people import People
 from api.calls.swapi import Swapi
 from api.client import Client
+from api.validator import Validator
+from common.logger import logging as log
+from requests import Response
 
-
-class Call:
+class Call(Validator):
     def __init__(self):
 
         self.client = Client
         self.people = People(self)
         self.swapi = Swapi(self)
 
+    @log("Get by endpoint")
+    @staticmethod
+    def get_by_endpoint(self, url) -> Response:
+        response = self.client.request(
+            method = "GET",
+            url = url,
+        )
+        return self.structure(response)
